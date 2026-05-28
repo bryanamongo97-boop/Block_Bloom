9fda648db4dd3fd4a0fe858d11fe972c4fce34af
bryanamongo <bryanamongo97@gmail.com>
2026-05-28 17:05:55 +0800

    try

--- Block_Bloom.cpp
+++ Block_Bloom.cpp
@@ -1,2 +1,16 @@
 #include <iostream>
 #include <vector>
+
+/*andandkanfkjzsbfjabsjfba,msc kjzbkjcKJSfjBC<SgzYJCbkZBcsjavskcnas
+sklhKJLHSFkjaKSJfkjabfjkANjvnnLKcjaLKScjfKAJBsfjkansckjbaScjbSAKJS
+kaudhasjcnzklhuasklczoifhlznscjzsodyialdmkjjdfslkajclkf;saf
+*/
+
+int main() {
+    std::vector<int> numbers = {1, 2, 3, 4, 5};
+    for (const auto& num : numbers) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+    return 0;
+} //trial pakibura hahahahha
